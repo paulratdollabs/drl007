aed90710a71cb78313254c2b156b99ece9aba29b
Robertson Paul <paulr@dollabs.com>
2023-11-27 17:55:16 -0500

    Put back the accidentally deleted deps.edn file Fixed the python plant interface to handle rendering

--- test-plants/gym_plant.py
+++ test-plants/gym_plant.py
@@ -67,12 +67,15 @@ class Rmq:
         self.done = False
         self.last_rmq_call_back = time.time()
 
+    envname = None
+    humanmode = False
+
     def make_env(self, msg):
         self.plant.started(msg)
-        envname, = msg['args']
+        self.envname, = msg['args']
         print('gym make: ', envname)
         #self.env=gym.make(envname, render_mode='human')
-        self.env=gym.make(envname) # Until we can figure out how to get the old functionality with the render_more +++
+        self.env=gym.make(envname)
         self.plant.finished(msg)
         #print('make_env, env=', self.env)
         self.publish_data_obs_rmq()
@@ -81,15 +84,32 @@ class Rmq:
         self.plant.started(msg)
         # no args for reset -- alt, = msg['args']
         if not self.env==None:
+            if self.humanmode:
+                self.env.close()             # Close the rendering
+                self.env = gym.make(envname) # Make a new envinonment without rendering
+                self.humanmode = False
             self.gym_new_state=self.env.reset()
             #print('reset') #, alt
             self.publish_state_obs_rmq()
         self.plant.finished(msg)
         #print('done reset')
 
+    def close(self, msg):
+        self.plant.started(msg)
+        # no args for reset -- alt, = msg['args']
+        if not self.env==None:
+            self.env.close()
+            self.env=None
+            self.publish_state_obs_rmq()
+        self.plant.finished(msg)
+        #print('done close')
+
     def render(self, msg):
         self.plant.started(msg)
         # no args for render -- alt, = msg['args']
+        self.env.close() # close the existing environment
+        self.env=gym.make(self.envname, render_mode='human')
+        self.humanmode = True
         self.env.render()
         #print('render') #, alt
         self.plant.finished(msg)
